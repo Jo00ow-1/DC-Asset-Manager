fa2429c178975447793d3fc07a1b4df91df810f6
josh.jh <josh.jh@joshjhui-MacBookPro.local>
2026-07-07 18:50:57 +0900

    Implement login redirect and dashboard

--- server.js
+++ server.js
@@ -6,4 +6,16 @@ app.listen(3000, () => {
 });
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
+app.post("/login", (req, res) => {
+    console.log("로그인 요청이 들어왔습니다.");
+    console.log(req.body);
+    if (req.body.id === "josh" &&
+        req.body.password === "1234"
+     ) {
+        res.redirect("dashboard.html");
+    } else {
+        res.send("로그인 실패");
+    }
+});
